c745de1f179f19c1b7c491fa1d9c859ab7c67b7b
Krishna-Teja732 <66668054+Krishna-Teja732@users.noreply.github.com>
2020-11-03 16:20:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package com.company;
 import java.io.*;
-import java.util.Scanner;
 
 class Student{
     private String studName ;
@@ -19,9 +18,9 @@ class Student{
 
 class Course{
     public static int numberOfSubjects=0;
-    public String subName ;
-    public String subCode ;
-    public Student[] obj = new Student[100];
+    private String subName ;
+    private String subCode ;
+    private Student[] obj = new Student[100];
 
     Course(String sub){
         String[] temp_lst  = sub.split(" - ");
@@ -29,7 +28,7 @@ class Course{
         subName = temp_lst[1];
     }
 
-    public static void setNumberOfSubjects() throws IOException {
+    public static void setNumberOfSubjects() throws IOException,FileNotFoundException {
         BufferedReader sc = new BufferedReader(new FileReader("D:\\3rd Sem psg tech\\OOPS\\Attend.csv"));
         String[] data_lst;
         String line= sc.readLine();
@@ -43,6 +42,9 @@ class Course{
         sc.close();
     }
 
+    public void setAttendance(String name,String rollnum,int[] temp,int studno){
+        obj[studno] = new Student(name, rollnum, temp);
+    }
     public void display(){
         System.out.println( subName+" "+subCode);
         for(Student s:obj)
@@ -51,20 +53,26 @@ class Course{
     }
 }
 
-class Main{
-    public static void main(String[] args)   throws IOException{
-        Course.setNumberOfSubjects();
-        Course[] sub_lst = new Course[Course.numberOfSubjects];
+class Readdata {
+    static Course[] sub_lst;
 
+
+    static void readData() throws IOException{
+        try{
+            Course.setNumberOfSubjects();}
+        catch (Exception e){
+            System.out.println(e);
+        }
+        sub_lst = new Course[Course.numberOfSubjects];
 
         BufferedReader sc = new BufferedReader(new FileReader("C:\\Users\\Teja\\Downloads\\Attend.csv"));
-        String line= sc.readLine();
+        String line = sc.readLine();
         String[] data_lst;
         data_lst = line.split(",");
         //reading subject names form file to class and instantiating sub list
-        int index=0;
-        for(String s: data_lst) {
-            if(!s.isEmpty() && !s.equals("September 2020")){
+        int index = 0;
+        for (String s : data_lst) {
+            if (!s.isEmpty() && !s.equals("September 2020")) {
                 sub_lst[index] = new Course(s);
                 index++;
             }
@@ -74,23 +82,29 @@ class Main{
         sc.readLine();
         sc.readLine();
         line = sc.readLine();
-        int studno=0;
-        while(line!=null){
+        int studno = 0;
+        while (line != null) {
             data_lst = line.split(",");
-            if(data_lst.length==0)
+            if (data_lst.length == 0)
                 break;
             int[] temp = new int[4];
-            int count=0;
-            while(count<Course.numberOfSubjects){
-                for(int i=2+(count*4), ind=0 ;i<6+(count*4);i++,ind++)
-                    temp[ind]= Integer.parseInt(data_lst[i]);
-                sub_lst[count].obj[studno] = new Student(data_lst[0],data_lst[1],temp);
+            int count = 0;
+            while (count < Course.numberOfSubjects) {
+                for (int i = 2 + (count * 4), ind = 0; i < 6 + (count * 4); i++, ind++)
+                    temp[ind] = Integer.parseInt(data_lst[i]);
+                sub_lst[count].setAttendance(data_lst[0], data_lst[1], temp,studno);
                 count++;
             }
             line = sc.readLine();
             studno++;
         }
-        sub_lst[1].display();
         sc.close();
+    }
+}
+
+class Main{
+    public static void main(String[] args)   throws IOException{
+        Readdata.readData();
+        Readdata.sub_lst[0].display();
     }
 }
